7111d6902f08bef638596e8da045b3f2c421dc7e
Kreviuz <benderbroun@gmail.com>
2014-05-02 17:57:55 +0300

    Resources extension.

---
+++ Diploma/Diploma/Extensions/ResourceExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace Diploma.Extensions
+{
+	public static class ResourceExtensions
+	{
+		public static string GetLocalResources(this WebViewPage page, string key)
+		{
+			return page.ViewContext.
+		}
+	}
+}
