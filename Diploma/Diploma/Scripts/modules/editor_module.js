b96ed6a369bfdbd70c3bfa7d2d2615b6dd2ac4d9
kreviuz <benderbroun@gmail.com>
2014-05-22 00:04:54 +0300

    CKEditor update -deleted old data

--- Diploma/Diploma/Scripts/modules/editor_module.js
+++ Diploma/Diploma/Scripts/modules/editor_module.js
@@ -2,27 +2,36 @@
     window.root = {};
     window.root.EditorModule = function (settings) {
         var module = {
+            settings: {
+            },
             init: function () {
                 var self = this;
-                self.settings = settings;
+                self.settingsInstance = $.extend({}, self.settings, settings);
                 self.initClick();
             },
             initEditor: function () {
                 var self = this;
-                $(self.settings.Selectors.Editor).ckeditor();
+                var editor = $(self.settingsInstance.Selectors.Editor);
+                var content = editor.html();
+                editor.html("<textarea />");
+                var ckEditor = editor.find("textarea:first").ckeditor({ language: 'ru' }).editor;
+                ckEditor.setData(content);
+                self.settingsInstance.ckEditor = ckEditor;
             },
             initClick: function () {
                 var self = this;
-                $(self.settings.Selectors.ButtonRedact).click(function () {
-                    self.initEditor(self.settings.Selectors.Editor);
-                    $(self.settings.Selectors.ButtonRedact).hide();
-                    $(self.settings.Selectors.ButtonSave).show();
+                var buttonRedact = $(self.settingsInstance.Selectors.ButtonRedact);
+                var buttonSave = $(self.settingsInstance.Selectors.ButtonSave);
+                buttonRedact.click(function () {
+                    self.initEditor(self.settingsInstance.Selectors.Editor);
+                    buttonRedact.hide();
+                    buttonSave.show();
                 });
-                $(self.settings.Selectors.ButtonSave).click(function () {
-                    var data = { data: $(self.settings.Selectors.Editor).html(), path: self.settings.Path };
+                buttonSave.click(function () {
+                    var data = { data: self.settingsInstance.ckEditor.getData(), path: self.settingsInstance.Path };
                     var xhr = $.ajax({
                         type: 'POST',
-                        url: self.settings.UrlPostData,
+                        url: self.settingsInstance.UrlPostData,
                         data: JSON.stringify(data),
                         dataType: "json",
                         contentType: 'application/json; charset=UTF-8'
